e031653791b1b86841cf82e72acdca7469384c8d
Elielton Agostinho <elieltonagostinho@edu.unifor.br>
2021-11-05 11:26:57 -0300

    Ajuste na api

--- app/controllers/dashboard_aluno_controller.rb
+++ app/controllers/dashboard_aluno_controller.rb
@@ -1,4 +1,6 @@
 class DashboardAlunoController < ApplicationController
+  PRODUCAO = 'https://unipointapi.herokuapp.com'
+  HOMOLOGACAO = 'http://localhost:5000'
   def index
     if session[:user].nil? == true or session[:user] == ''
       session.delete(:user)
@@ -8,11 +10,43 @@ class DashboardAlunoController < ApplicationController
 
       getUsuario(session[:user])
 
-      getDisciplina(session[:user])
+      d = DateTime.now
+      dia = d.strftime("%A")
+      hora = d.strftime("%H")
 
-      podeMarcar(@id_disciplina)
+      semanaD = {"Monday" => 2,"Tuesday" => 3,"Wednesday" => 4, "Thursday" => 5, "Friday" => 6, "Saturday" => 7}
 
-      getPonto(session[:user],@id_disciplina)
+      turno = ''
+      case hora.to_i # a_variable is the variable we want to compare
+      when 6..12 then turno = "M"
+      when 13..18  then turno = "T"
+      when 19..23 then turno = "N"
+      end
+
+      periodo = ''
+      case hora.to_f # a_variable is the variable we want to compare
+      when 6,30..8,59 then periodo = "AB"
+      when 9..12,59  then periodo = "CD"
+      when 13..14,59  then periodo = "AB"
+      when 15..17,29  then periodo = "CD"
+      when 17,30..18,29 then periodo = "EF"
+      when 18,30..20,59 then periodo = "AB"
+      when 21..22,30 then periodo = "CD"
+      end
+
+      codigoDisciplina = turno+semanaD[dia].to_s+periodo
+      puts codigoDisciplina
+
+      getDisciplina(session[:user],codigoDisciplina)
+
+      if @semDic == 1 then
+        podeMarcar(@id_disciplina)
+
+        getPonto(session[:user],@id_disciplina)
+
+      end
+
+
 
     end
   end
@@ -45,21 +79,27 @@ class DashboardAlunoController < ApplicationController
     @nome = nome[0] + ' ' + nome[1]
   end
 
-  def getDisciplina(matricula)
+  def getDisciplina(matricula,codDisc)
     require 'http'
 
-    response = HTTP.post("https://unipointapi.herokuapp.com/getDisciplina", :form => {'matricula' => matricula})
+    response = HTTP.post("https://unipointapi.herokuapp.com/getDisciplina", :form => {'matricula' => matricula, "cd_disciplina" => codDisc})
     response.body # retorna um objeto representando a resposta
     response.code # retorna o código HTTP da resposta, e.g. 404, 500, 200
 
     r = JSON.parse(response.body)
 
     retorno = r[0]
-    #puts retorno["id_disciplina"]
+    puts "Retorno: ",r[0]
+    @semDic = 1
     #podeMarcar(retorno["id_disciplina"])
-    @id_disciplina = retorno["id_disciplina"]
-    @disciplina = retorno["NOME"]
-    @horario = retorno["COD_DISC"]
+    if retorno["vazio"] then
+      @semDic = retorno["vazio"]
+    else
+      @id_disciplina = retorno["id_disciplina"]
+      @disciplina = retorno["NOME"]
+      @horario = retorno["COD_DISC"]
+    end
+
 
   end
 
